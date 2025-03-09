b3194e601dcc36229415586a49196a65576196f1
odysseia06 <91327287+odysseia06@users.noreply.github.com>
2025-03-09 10:40:52 +0300

    Implementing the number guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use ferris_says::say;
-use std::io::{stdout, BufWriter};
+use rand::Rng;
+use std::io::{self, stdout, BufWriter};
 
 fn main() {
     let stdout = stdout();
@@ -7,4 +8,37 @@ fn main() {
     let width = message.chars().count();
     let mut writer = BufWriter::new(stdout.lock());
     say(&message, width, &mut writer).unwrap();
+
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a number!");
+                continue;
+            }
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
